b4f55bc204ca65002d40a3d6c8e4ff9930663095
Melissa <melissaguillen70@yahoo.com>
2023-08-28 16:12:56 -0700

    Initialize Project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <>
-     
+     <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+     </div>
     </>
   )
 }
