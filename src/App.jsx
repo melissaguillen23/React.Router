9c88ad722a5281402a5ed35b336e36ac77e92cb8
Melissa <melissaguillen70@yahoo.com>
2023-08-28 23:25:50 -0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue"
+import Red from "./components/Red"
+import Home from "./components/Home"
 
 function App() {
 
   return (
     <>
      <div id="container">
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/">Home</Link>
+      </div>
       <div id="main-section">
         <Routes>
-          <Route path="/blue" element={<h1>Blue</h1>} />
-          <Route path="/red" element={<h1>Red</h1>} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
      </div>
